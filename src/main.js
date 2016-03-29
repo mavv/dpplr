41fefb44fa9fae88f4a2327c68f96941dad16fde
mavv <sickmav@gmail.com>
2016-03-29 23:24:48 +0300

    some more configs & pic

--- src/main.js
+++ src/main.js
@@ -4,5 +4,17 @@ const message = (from) => `some message from ${from}.`;
 const log =  () => {
   console.log('bla bla bla\n');
   console.log(message('outer space'));
+  console.log('aaaaaaa');
 };
 export default log();
+//
+//
+// "scripts": {
+//     "start": "npm run watch-sass & npm run server",
+//     "postinstall": "npm run build-sass; grunt default",
+//     "test": "karma start karma.conf.js",
+//     "server": "live-server",
+//     "autoprefixer": "postcss -u autoprefixer -r  assets/styles/main.css",
+//     "build-sass": "node-sass -o assets/styles/ src/sass/main.scss",
+//     "watch-sass": "nodemon -e scss -x \"npm run build-sass && npm run autoprefixer\""
+//   },
